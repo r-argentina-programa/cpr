cd5b76afc013c766d2036e91acb45f29a3156ec5
Olucho <luchoorfino@gmail.com>
2021-01-11 16:46:06 -0300

    Adds product controller basic save function

--- server/src/module/product/controller/productController.js
+++ server/src/module/product/controller/productController.js
@@ -1,15 +1,38 @@
+const { fromDataToEntity } = require('../mapper/mapper');
 module.exports = class ProductController {
+  /**
+   * @param  {import("../service/productService")} ProductService
+   */
   constructor(ProductService, UploadMiddleware) {
     this.ProductService = ProductService;
     this.UploadMiddleware = UploadMiddleware;
   }
-
+  /**
+   * @param  {import("express".Application)} app
+   */
   configureRoutes(app) {
     const ROUTE = '/product';
-    app.get(`${ROUTE}`, this.test.bind(this));
+    app.get(`${ROUTE}`, this.UploadMiddleware.single('file'), this.save.bind(this));
   }
+  /**
+   * @param  {import("express".Request)} req
+   * @param  {import("express").Response} res
+   */
+  async save(req, res) {
+    const { filename } = req.file;
+    const { name, defaultPrice, description } = req.body;
 
-  test(req, res) {
-    console.log('Testing Funct when called');
+    const productImageURL = `${process.env.APP_URL}/files/${filename}`;
+    try {
+      const productData = fromDataToEntity({
+        name,
+        defaultPrice,
+        description,
+        imageSrc: productImageURL,
+      });
+      let newProduct = await this.ProductService.save(productData);
+    } catch (error) {
+      console.log(error);
+    }
   }
 };
