61a2b6219f9027085ee895f6e52127b9b2ce745d
Olucho <luchoorfino@gmail.com>
2021-01-11 14:49:30 -0300

    Add product module file

---
+++ server/src/module/product/module.js
@@ -0,0 +1,11 @@
+const ProductController = require('./controller/productController');
+const ProductService = require('./service/productService');
+const ProductRepository = require('./repository/productRepository');
+const ProductModel = require('./model/productModel');
+
+module.exports = {
+  ProductController,
+  ProductService,
+  ProductRepository,
+  ProductModel,
+};
