5ccd5f0cc08b2ffba58af3054748846da5503f72
Olucho <luchoorfino@gmail.com>
2021-01-11 15:40:22 -0300

    Setup Product module Boostrap

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,16 @@
-const env = require("dotenv");
-const express = require("express");
+const env = require('dotenv');
+const express = require('express');
 const app = express();
-//
+
+const configureDI = require('./config/di');
+const { initProductModule } = require('./module/product/module');
+
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
+
+const container = configureDI();
+initProductModule(app, container);
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
--- server/src/module/product/module.js
+++ server/src/module/product/module.js
@@ -3,9 +3,22 @@ const ProductService = require('./service/productService');
 const ProductRepository = require('./repository/productRepository');
 const ProductModel = require('./model/productModel');
 
+/**
+ * @param  {import("rsdi").IDIContainer} container
+ * @param  {import("express").Application} app
+ */
+function initProductModule(app, container) {
+  /**
+   * @type {import("./controller/productController")} 'ProductController'
+   */
+  const controller = container.get('ProductController');
+  controller.configureRoutes(app);
+}
+
 module.exports = {
   ProductController,
   ProductService,
   ProductRepository,
   ProductModel,
+  initProductModule,
 };
