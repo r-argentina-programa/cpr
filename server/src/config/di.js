41480d21cf8ff6ae560c1c9507dd286138a23497
Olucho <luchoorfino@gmail.com>
2021-01-11 17:32:15 -0300

    deletes unused sequelize option

--- server/src/config/di.js
+++ server/src/config/di.js
@@ -17,7 +17,6 @@ function configureSequelizeDatabase() {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    storage: process.env.DB_PATH,
   });
 }
 
