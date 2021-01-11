1a83d2d4019a091c4123683e43ca50f69cd44319
Olucho <luchoorfino@gmail.com>
2021-01-11 16:44:46 -0300

    Setups dotenv and public files

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
-const env = require('dotenv');
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const configureDI = require('./config/di');
 const { initProductModule } = require('./module/product/module');
@@ -8,7 +9,7 @@ const { initProductModule } = require('./module/product/module');
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
-
+app.use('/files', express.static(path.resolve(__dirname, '..', '..', 'public', 'uploads')));
 const container = configureDI();
 initProductModule(app, container);
 
