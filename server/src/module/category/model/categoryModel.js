e18524495ee8f8aed90f00100238dfcdbb8cd694
k- <mahat.mejor@gmail.com>
2021-01-13 06:08:23 -0600

    adds category model

---
+++ server/src/module/category/model/categoryModel.js
@@ -0,0 +1,42 @@
+const { Sequelize, Model, DataTypes } = require('sequelize');
+
+module.exports = class CategoryModel extends Model {
+  /**
+   * @param {import('sequelize').Sequelize} sequelizeInstance
+   * @returns {typeof CategoryModel}
+   */
+  static setup(sequelizeInstance) {
+    CategoryModel.init(
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+          unique: true,
+        },
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        updatedAt: {
+          type: DataTypes.DATE,
+          defaultValue: Sequelize.NOW,
+        },
+        createdAt: {
+          type: DataTypes.DATE,
+          defaultValue: Sequelize.NOW,
+        },
+      },
+      {
+        sequelize: sequelizeInstance,
+        modelName: 'Category',
+        underscored: true,
+        paranoid: true,
+        timestamps: false,
+      }
+    );
+
+    return CategoryModel;
+  }
+};
