f95ed4f6511b64e1886b1ccb0dbcc8597c754176
k- <mahat.mejor@gmail.com>
2021-01-13 09:33:41 -0600

    rename variable

--- server/src/module/category/controller/categoryController.js
+++ server/src/module/category/controller/categoryController.js
@@ -21,7 +21,7 @@ module.exports = class CategoryController {
     app.get(`${ROUTE}/edit/:categoryId`, this.edit.bind(this));
     app.get(`${ROUTE}/add`, this.add.bind(this));
     app.post(`${ROUTE}/save`, this.save.bind(this));
-    app.post(`${ROUTE}/delete/:categoryId`, this.delete.bind(this));
+    app.get(`${ROUTE}/delete/:categoryId`, this.delete.bind(this));
   }
 
   /**
@@ -29,10 +29,10 @@ module.exports = class CategoryController {
    * @param {import('express').Response} res
    */
   async index(req, res) {
-    const categories = await this.categoryService.getAll();
+    const categoriesList = await this.categoryService.getAll();
     res.render(`${this.CATEGORY_VIEWS}/index.njk`, {
       title: 'Category List',
-      categories,
+      categoriesList,
     });
   }
 
