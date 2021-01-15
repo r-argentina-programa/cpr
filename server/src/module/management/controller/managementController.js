c7d13dd3e56e4968cc6413859598b30276643039
Hernán Peralta <alegodemencia@gmail.com>
2021-01-15 17:01:18 -0300

    Add suppport for React build

--- server/src/module/management/controller/managementController.js
+++ server/src/module/management/controller/managementController.js
@@ -9,9 +9,6 @@ module.exports = class ManagementController {
   configureRoutes(app) {
     const ROUTE = this.ROUTE_BASE;
 
-    app.get('/', (req, res) => {
-      res.send('Hello World! - Testing Codeship');
-    });
     app.get(`${ROUTE}/brands/all`, this.allBrands.bind(this));
     app.get(`${ROUTE}/brand/:id`, this.brand.bind(this));
     app.get(`${ROUTE}/categories/all`, this.allCategories.bind(this));
