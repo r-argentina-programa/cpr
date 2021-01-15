e95cadf290add3d1fc51f7f3eb36d08475008896
Olucho <luchoorfino@gmail.com>
2021-01-15 15:57:11 -0300

    Displays the image of the product correctly

--- client/src/components/productsList/index.js
+++ client/src/components/productsList/index.js
@@ -5,6 +5,8 @@ import Button from "react-bootstrap/Button";
 import TimeAgo from "react-timeago";
 import buildFormatter from "react-timeago/lib/formatters/buildFormatter";
 import englishString from "react-timeago/lib/language-strings/en";
+import { useEffect, useState } from "react";
+import ab2str from "arraybuffer-to-string";
 
 const formatter = buildFormatter(englishString);
 
@@ -16,12 +18,18 @@ const TimeStyle = styled.span`
 `;
 
 export default function ProductsList({ product }) {
+  const [image, setImage] = useState("");
+  useEffect(() => {
+    const uint8 = new Uint8Array(product.imageSrc.data);
+    setImage(ab2str(uint8));
+  }, [product.imageSrc.data]);
+
   return (
     <>
       <Card style={{ width: "15rem", height: "10rem" }}>
         <Card.Body>
           <Card.Title>{product.name}</Card.Title>
-          <Card.Img variant="top" src={product.imageSrc} />
+          <Card.Img variant="top" src={`data:image/png;base64, ${image}`} />
           <TimeStyle>
             <TimeAgo date={`${product.createdAt}`} formatter={formatter} />
             <br />
