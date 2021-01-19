404d879d57ef636ae14817372bd4acbca5d2d2fb
Hernán Peralta <alegodemencia@gmail.com>
2021-01-19 16:17:42 -0300

    Fix routes

--- server/src/app.js
+++ server/src/app.js
@@ -35,7 +35,7 @@ initManagementModule(app, container);
 const mainDb = container.get('Sequelize');
 mainDb.sync();
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
