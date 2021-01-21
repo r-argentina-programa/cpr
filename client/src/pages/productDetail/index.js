72b1174f7050262508a6a16e245c4cda1bf992ee
Olucho <luchoorfino@gmail.com>
2021-01-21 14:49:49 -0300

    Improve layout on product detail page

--- client/src/pages/productDetail/index.js
+++ client/src/pages/productDetail/index.js
@@ -4,7 +4,13 @@ import { useContext, useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
 import ab2str from "arraybuffer-to-string";
 import { ProductContext } from "../../store/products/productContext";
-
+import {
+  Container,
+  ImageContainer,
+  ProductDescription,
+  RightColumnContainer,
+  ProductPrice,
+} from "./styles";
 export default function ProductDetail() {
   const { id } = useParams();
   const [image, setImage] = useState("");
@@ -21,26 +27,53 @@ export default function ProductDetail() {
     }
     setImage(ab2str(uint8));
   }, [product]);
+
+  console.log(product);
   return (
     <>
       <Header />
       {!product ? (
         <p>Loading.. Please wait</p>
       ) : (
-        <div className="container-fluid">
-          <Card className=" text-black">
-            <Card.Img
-              src={`data:image/png;base64, ${image}`}
-              alt="Card image"
-              style={{ width: "30%", height: "10rem", alignSelf: "center" }}
-            />
-            <Card.Title className="text-center">{product.name}</Card.Title>
-            <Card.Text className="text-center">{product.description}</Card.Text>
-            <Card.Text className="text-center">
-              ${product.defaultPrice}
-            </Card.Text>
-          </Card>
-        </div>
+        <Container>
+          <ImageContainer>
+            <img src={`data:image/png;base64, ${image}`} />
+          </ImageContainer>
+
+          <RightColumnContainer>
+            <ProductDescription>
+              {product.categories &&
+                product.categories.map((category) => (
+                  <span>{category.name}</span>
+                ))}
+              <h1>{product.name}</h1>
+              <p>{product.description}</p>
+            </ProductDescription>
+
+            <ProductPrice>
+              {product.discount ? (
+                <>
+                  <span style={{ textDecoration: "line-through" }}>
+                    ${product.defaultPrice}
+                  </span>
+                  <span
+                    style={{
+                      color: "red",
+                      border: "1px solid red",
+                      padding: ".4rem",
+                    }}
+                  >
+                    ${product.discount.finalPrice}
+                  </span>
+                </>
+              ) : (
+                <span>${product.defaultPrice}</span>
+              )}
+              <br />
+              <a href="#">Add to Cart</a>
+            </ProductPrice>
+          </RightColumnContainer>
+        </Container>
       )}
     </>
   );
