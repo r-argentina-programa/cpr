7ce0b6116b238fdfbb0eb404ba1b5f3081d8d505
Alexis Toranzo <alextoranzo3@gmail.com>
2021-01-22 13:13:22 -0300

    Validate category discounts

--- server/src/module/product/service/productService.js
+++ server/src/module/product/service/productService.js
@@ -1,23 +1,56 @@
+/* eslint-disable class-methods-use-this */
 const ProductIdNotDefinedError = require('../error/ProductIdNotDefinedError');
 const ProductNotDefinedError = require('../error/ProductNotDefinedError');
 const Product = require('../entity/entity');
+const { calculatePrice } = require('../../management/utils/calculatePrice');
 
 module.exports = class ProductService {
   /**
    * @param  {import("../repository/productRepository")} ProductRepository
    */
-  constructor(ProductRepository) {
+  constructor(ProductRepository, CategoryService, DiscountService) {
     this.ProductRepository = ProductRepository;
+    this.CategoryService = CategoryService;
+    this.DiscountService = DiscountService;
   }
 
   /**
    * @param {Product} product
    */
-  async save(product, categories, discounts) {
+  async save(product, categoriesIds, discountsIds) {
     if (!(product instanceof Product)) {
       throw new ProductNotDefinedError();
     }
-    return this.ProductRepository.save(product, categories, discounts);
+
+    await this.validateCategoriesDiscounts(product, categoriesIds);
+
+    return this.ProductRepository.save(product, categoriesIds, discountsIds);
+  }
+
+  async validateCategoriesDiscounts(product, categoriesIds) {
+    const categories = await this.CategoryService.getByIds(categoriesIds);
+    categories.forEach((category) => {
+      category.discounts.forEach((discount) => {
+        const price = calculatePrice(discount, product.defaultPrice);
+        if (price.finalPrice <= 0) {
+          throw new Error(
+            `No se puede guardar el producto porque al aplicarle el/los descuento de la/s categorias su precio final es ${price.finalPrice}`
+          );
+        }
+      });
+    });
+  }
+
+  async validateProductsDiscounts(product, discountsIds) {
+    const discounts = await this.DiscountService.getByIds(discountsIds);
+    discounts.map((discount) => {
+      const price = calculatePrice(discount, product.defaultPrice);
+      if (price.finalPrice <= 0) {
+        throw new Error(
+          `No se puede guardar el producto porque al aplicarle el/los descuentos del producto su precio final es ${price.finalPrice}`
+        );
+      }
+    });
   }
 
   /**
