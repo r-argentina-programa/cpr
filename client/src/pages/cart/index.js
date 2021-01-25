b69202b55debe1b06854dc9c13ada19fb4b2cedd
Olucho <luchoorfino@gmail.com>
2021-01-25 15:59:48 -0300

    Switch page layout to bootstrap table component

--- client/src/pages/cart/index.js
+++ client/src/pages/cart/index.js
@@ -3,13 +3,13 @@
 /* eslint-disable no-const-assign */
 /* eslint-disable no-unused-vars */
 import { useEffect, useState } from 'react';
-
 import { Link } from 'react-router-dom';
-
+import { Table } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 import ab2str from 'arraybuffer-to-string';
+import styled from 'styled-components/macro';
 
 import Header from '../../components/header';
-import { Cart, Product, Name, Content, Footer } from './styles';
 
 export default function ProductDetail() {
   let [amount, setAmount] = useState(1);
@@ -35,61 +35,94 @@ export default function ProductDetail() {
     const uint8 = new Uint8Array(imageSrc);
     return ab2str(uint8);
   }
+
+  const Container = styled.div`
+    img {
+      max-width: 100%;
+      max-height: 10rem;
+    }
+    .price {
+      font-size: 1.5rem;
+      text-align: center;
+    }
+    .price-discount {
+      color: lightsalmon;
+    }
+    .brand {
+      font-size: 0.8rem;
+      color: #358ed7;
+      letter-spacing: 1px;
+      text-transform: uppercase;
+      text-decoration: none;
+      margin-right: 0.9rem;
+    }
+  `;
+
+  console.log(products);
   return (
     <>
       <Header />
 
-      {products ? (
-        products.map((product) => (
-          <Cart className="cart" key={product.id}>
-            <Product className="product">
-              <header>
-                <img
-                  alt="product"
-                  src={`data:image/png;base64, ${configureImage(product.imageSrc.data)}`}
-                />
-              </header>
-
-              <Content className="content">
-                <Name>{product.name}</Name>
-                {product.description}
-                <div style={{ top: '43px' }} className="type small">
-                  <Link to={`/brand/${product.brand.id}`}>{product.brand.name}</Link>
-                </div>
-              </Content>
-
-              <Footer className="content">
-                <button type="button" className="qt-minus" onClick={changeValue}>
-                  -
-                </button>
-                <span className="qt">{amount}</span>
-                <button type="button" className="qt-plus" onClick={changeValue}>
-                  +
-                </button>
-
-                {product.discount.finalPrice ? (
-                  product.discount.type === 'Fixed' ? (
-                    <>
-                      <h2 className="full-price">${product.discount.finalPrice}</h2>
-                      <span className="discount">-${product.discount.value} OFF</span>
-                    </>
-                  ) : (
-                    <>
-                      <h2 className="full-price">${product.discount.finalPrice}</h2>
-                      <span className="discount">-%{product.discount.value} OFF</span>
-                    </>
-                  )
-                ) : (
-                  <h2 className="default-price">${product.defaultPrice}</h2>
-                )}
-
-                <h2 className="price">${product.defaultPrice}</h2>
-              </Footer>
-            </Product>
-          </Cart>
-        ))
+      {products.length === 0 ? (
+        <h1>You do not have any products added to the cart!</h1>
       ) : (
-        <p>Loading... please wait</p>
+        <Container>
+          <Table striped hover>
+            <thead>
+              <tr>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Brand</th>
+                <th>Categories</th>
+                <th>Description</th>
+                <th>Default Price</th>
+                <th>Final Price (with Discount)</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr>
+                  <td>
+                    <img
+                      src={`data:image/png;base64, ${configureImage(product.imageSrc.data)}`}
+                      alt={product.name}
+                    />
+                  </td>
+                  <td>{product.name}</td>
+                  <Link to={`/brand/${product.brand.id}`} className="brand">
+                    <td>{product.brand.name}</td>
+                  </Link>
+                  {product.categories.length !== 0 ? (
+                    product.categories.map((category) => <td key={category.id}>{category.name}</td>)
+                  ) : (
+                    <td>No categories in this product</td>
+                  )}
+                  <td>{product.description}</td>
+                  <td className="price">${product.defaultPrice}</td>
+                  {product.discount ? (
+                    product.discount.type === 'Fixed' ? (
+                      <td className="price price-discount">
+                        ${product.discount.finalPrice} (-${product.discount.value} OFF)
+                      </td>
+                    ) : (
+                      <td className="price price-discount">
+                        ${product.discount.finalPrice} (-%{product.discount.value} OFF)
+                      </td>
+                    )
+                  ) : (
+                    <td>This product has no discount.</td>
+                  )}
+                  <td>
+                    <Button variant="danger" type="button" className="button">
+                      Delete
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Container>
       )}
     </>
   );
