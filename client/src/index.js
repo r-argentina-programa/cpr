d0dba63bbda8433118f32acc96213ec9884614d1
Olucho <luchoorfino@gmail.com>
2021-01-27 17:16:51 -0300

    Add Global Styles

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import GlobalStyles from './globalStyles';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
