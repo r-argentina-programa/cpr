742d7a730a84e034b1308ef852f61d9ae58bfc53
Olucho <luchoorfino@gmail.com>
2021-01-27 16:47:06 -0300

    Fix Grid layout

--- client/src/pages/main/styles.js
+++ client/src/pages/main/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components/macro';
 export const ListContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-grid-gap: 0.2rem;
+  grid-gap: 0.2rem;
 `;
 
 export const NavContainer = styled.div`
