a195037d14a9250a35bad371ca57bea47deaf894
Olucho <luchoorfino@gmail.com>
2021-01-29 12:32:37 -0300

    Moves Filter to Sidebar

--- client/src/pages/main/index.js
+++ client/src/pages/main/index.js
@@ -8,7 +8,7 @@ import Header from '../../components/header';
 import { ProductContext } from '../../store/products/productContext';
 import { BrandContext } from '../../store/brand/brandContext';
 import { CategoryContext } from '../../store/category/categoryContext';
-import { ListContainer, NavContainer, Title } from './styles';
+import { ListContainer, SidebarContainer, Container, ContentContainer } from './styles';
 
 export default function Main() {
   const { getAllProducts, products, getFilteredProducts, error } = useContext(ProductContext);
@@ -38,90 +38,96 @@ export default function Main() {
   return (
     <>
       <Header />
-      <NavContainer>
-        <span>Brands:</span>
-        {brands.map((brand) => (
-          <div className="item" key={brand.id}>
-            <label htmlFor={`brand-${brand.id}`}>{brand.name}</label>
-            <input
-              type="checkbox"
-              id={`brand-${brand.id}`}
-              value={brand.id}
-              onClick={(e) => {
-                if (activeBrands.includes(e.target.value)) {
-                  setActiveBrands(activeBrands.filter((id) => id !== e.target.value));
-                } else {
-                  setActiveBrands([...activeBrands, e.target.value]);
-                }
-              }}
-            />
-          </div>
-        ))}
-      </NavContainer>
-      <NavContainer>
-        <span>Categories:</span>
-        {categories.map((category) => (
-          <div className="item" key={category.id}>
-            <label htmlFor={`category-${category.id}`}>{category.name}</label>
-            <input
-              type="checkbox"
-              id={`category-${category.id}`}
-              value={category.id}
-              onClick={(e) => {
-                if (activeCategories.includes(e.target.value)) {
-                  setActiveCategories(activeCategories.filter((id) => id !== e.target.value));
-                } else {
-                  setActiveCategories([...activeCategories, e.target.value]);
-                }
-              }}
-            />
-          </div>
-        ))}
-      </NavContainer>
-      <NavContainer>
-        <div className="price-filter">
-          <label htmlFor="min-price">
-            Min Price:
-            <input
-              type="number"
-              id="min-price"
-              name="min-price"
-              min="0"
-              value={price.minPrice}
-              onChange={(e) => setPrice({ ...price, minPrice: e.target.value })}
-            />
-          </label>
-          <label htmlFor="max-price">
-            Max Price:
-            <input
-              type="number"
-              id="max-price"
-              min="0"
-              name="max-price"
-              value={price.maxPrice}
-              onChange={(e) => setPrice({ ...price, maxPrice: e.target.value })}
-            />
-          </label>
-          <Button
-            style={{ backgroundColor: '#0D6572', borderColor: '#0D6572' }}
-            onClick={() => handleFilter()}
-          >
-            Filter Products
-          </Button>
-        </div>
-      </NavContainer>
-      {error && <Alert variant="danger">{error}</Alert>}
-      <Title>See all the products Here!</Title>
-      <ListContainer className="container-fluid">
-        {products.map((product) => (
-          <CardsList
-            key={product.id}
-            item={product}
-            imageSrc={product.imageSrc.data}
-            link={`/product/${product.id}`}
-          />
-        ))}
-      </ListContainer>
+      <Container clasName="container">
+        <SidebarContainer>
+          <ul className="filters">
+            <li>
+              Brands:
+              {brands.map((brand) => (
+                <ul className="item" key={brand.id}>
+                  <label htmlFor={`brand-${brand.id}`}>{brand.name}</label>
+                  <input
+                    type="checkbox"
+                    id={`brand-${brand.id}`}
+                    value={brand.id}
+                    onClick={(e) => {
+                      if (activeBrands.includes(e.target.value)) {
+                        setActiveBrands(activeBrands.filter((id) => id !== e.target.value));
+                      } else {
+                        setActiveBrands([...activeBrands, e.target.value]);
+                      }
+                    }}
+                  />
+                </ul>
+              ))}
+            </li>
+            <li>
+              Categories:
+              {categories.map((category) => (
+                <ul className="item" key={category.id}>
+                  <label htmlFor={`category-${category.id}`}>{category.name}</label>
+                  <input
+                    type="checkbox"
+                    id={`category-${category.id}`}
+                    value={category.id}
+                    onClick={(e) => {
+                      if (activeCategories.includes(e.target.value)) {
+                        setActiveCategories(activeCategories.filter((id) => id !== e.target.value));
+                      } else {
+                        setActiveCategories([...activeCategories, e.target.value]);
+                      }
+                    }}
+                  />
+                </ul>
+              ))}
+            </li>
+            <div className="price-filter">
+              <label htmlFor="min-price">
+                Min Price:
+                <input
+                  type="number"
+                  id="min-price"
+                  name="min-price"
+                  min="0"
+                  value={price.minPrice}
+                  onChange={(e) => setPrice({ ...price, minPrice: e.target.value })}
+                />
+              </label>
+              <label htmlFor="max-price">
+                Max Price:
+                <input
+                  type="number"
+                  id="max-price"
+                  min="0"
+                  name="max-price"
+                  value={price.maxPrice}
+                  onChange={(e) => setPrice({ ...price, maxPrice: e.target.value })}
+                />
+              </label>
+              <Button
+                style={{ backgroundColor: '#0D6572', borderColor: '#0D6572' }}
+                onClick={() => handleFilter()}
+              >
+                Filter Products
+              </Button>
+            </div>
+          </ul>
+        </SidebarContainer>
+        {error && <Alert variant="danger">{error}</Alert>}
+
+        <ContentContainer>
+          <ListContainer className="container-fluid">
+            {products.map((product) => (
+              <CardsList
+                key={product.id}
+                item={product}
+                imageSrc={product.imageSrc.data}
+                link={`/product/${product.id}`}
+              />
+            ))}
+          </ListContainer>
+        </ContentContainer>
+      </Container>
     </>
   );
 }
