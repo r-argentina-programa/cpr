6755cbbb6fdaebcdc3ec730ee24b7a7e36aa29fa
k- <mahat.mejor@gmail.com>
2021-02-01 11:52:44 -0600

    Fix typo

--- client/src/globalStyles.js
+++ client/src/globalStyles.js
@@ -2,11 +2,10 @@ import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
     html{
-        min-width: 483px;
         margin: 0;
         padding: 0;
     }
     #root{
-        height: 100wh;
+        min-height: 100vh;
     }
 `;
